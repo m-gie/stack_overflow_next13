6168e80640d1f76a9ecb89b248f99e897e2f4b6d
jquan collins <jquancollins96@gmail.com>
2024-04-02 19:09:29 -0400

    good commits

--- app/context/ThemeProvider.tsx
+++ app/context/ThemeProvider.tsx
@@ -13,12 +13,16 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
   const [mode, setMode] = useState("");
 
   const handleThemeChange = () => {
-    if (mode === "dark") {
-      setMode("light");
-      document.documentElement.classList.add("light");
-    } else {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
       setMode("dark");
       document.documentElement.classList.add("dark");
+    } else {
+      setMode("light");
+      document.documentElement.classList.remove("dark");
     }
   };
 
@@ -37,7 +41,7 @@ export function useTheme() {
   const context = useContext(ThemeContext);
 
   if (context === undefined) {
-    throw new Error("useTheme must be used within ThemeProvider");
+    throw new Error("useTheme must be used within a ThemeProvider");
   }
 
   return context;
