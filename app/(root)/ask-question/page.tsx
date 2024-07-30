7b42c0c7140da92740e6a2d79b354db76a2a6166
jquan collins <jquancollins96@gmail.com>
2024-07-29 22:21:29 -0400

    question for and validation

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,7 +1,15 @@
+import Question from "@/app/components/forms/Question";
 import React from "react";
 
-const page = () => {
-  return <div>Ask Question</div>;
+const Page = () => {
+  return (
+    <div>
+      <h1 className=" h1-bold text-dark100_light900"> Ask question</h1>
+      <div className="mt-9">
+        <Question />
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default Page;
