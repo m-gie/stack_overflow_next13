1d068a69e2882648d23dbc4d78ba564c1c17caa2
jquan collins <jquancollins96@gmail.com>
2024-11-23 09:11:33 -0500

    new message

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -5,9 +5,9 @@ import { redirect } from "next/navigation";
 import React from "react";
 
 const Page = async () => {
-  // const { userId } = auth();
+  const { userId } = auth();
 
-  const userId = "123467890";
+  // const userId = "123467890";
   if (!userId) redirect("/sign-in");
 
   const mongoUser = await getUserById({ userId });
